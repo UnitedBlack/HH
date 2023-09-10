ed974de937b52f2368b5383cd7a10868a173914c
UnitedBlack <91415015+UnitedBlack@users.noreply.github.com>
2023-09-10 22:03:04 +0300

    Selenium

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
-from selenium import webdriver # это для бравзера
-from selenium.webdriver.common.by import By # это для парсинга
-import time # это на всякий случай для ожидания, но там встроенный метод есть его потом импортировать надо
-from selenium.webdriver.chrome.options import Options # это для опций
-# это счетчик итераций (это я просто жаваскриптер, он в жсе юзается и он там по умолчанию его не надо объявлять)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+
 CYCLE_INDEX = 0
-# Это создание опции хеадлесс чтобы оно не открывало каждый раз браузер
+
 chrome_options = Options()
 chrome_options.add_argument("--headless")
-# Экземпляр класса
+
+
 driver = webdriver.Chrome(options=chrome_options)
-# загрузка страницы
+actions = ActionChains(driver)
+
 print("Loading page...")
 driver.get("https://hh.ru/search/vacancy?text=Python&salary=&ored_clusters=true&enable_snippets=true&area=1")
-# это вот основной дроч
-# element = driver.find_elements(
-#     By.XPATH, '//span[@data-page-analytics-event="vacancy_search_suitable_item"]/@href')
-# get_vacancy_title = driver.find_elements(By.CLASS_NAME, 'serp-item__title') 
-# не забывай что надо использовать цикл фор как минимум потому что driver.find_elementS возвращает список
-# а список надо перебирать циклом фор
-# for i in element:
-#     CYCLE_INDEX += 1
-#     print(CYCLE_INDEX, i.text)
 
-# закрытие бравзера
+next_page = driver.find_element(By.XPATH, "//a[@data-qa='pager-next']")
+hrefs = next_page.get_attribute("href")
+actions.move_to_element(next_page).perform()
+
+
+vacancy_link = driver.find_elements(
+    By.XPATH, "//a[@class='serp-item__title']")
+# get_vacancy_title = driver.find_elements(By.CLASS_NAME, 'serp-item__title')
+
+for i in vacancy_link:
+    CYCLE_INDEX += 1
+    href = i.get_attribute("href")
+    print(f"{CYCLE_INDEX}. Вакансия: {i.text}, ссылка: {href}")
+
 driver.quit()
-# это похуй
+
 if __name__ == "__main__":
     pass
