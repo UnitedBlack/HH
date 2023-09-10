b78650e670eeefade9f18c10fd6287a08d39b157
UnitedBlack <91415015+UnitedBlack@users.noreply.github.com>
2023-09-10 17:40:05 +0300

    SOLENOID

--- main.py
+++ main.py
@@ -1,60 +1,19 @@
-import requests
-import lxml
-from bs4 import BeautifulSoup as bs
-import re
-from forhttp import cookies
-from forhttp import headers
-from forhttp import params
-from lxml import etree
-from lxml import html
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+CYCLE_INDEX = 0
+driver = webdriver.Chrome()
 
-for_searching = "Маркетолог"
-text_string = params['text'] = for_searching
+driver.get("https://hh.ru/search/vacancy?text=Python&salary=&ored_clusters=true&enable_snippets=true&area=1")
+time.sleep(5)
+element = driver.find_elements(
+    By.XPATH, '//span[@data-page-analytics-event="vacancy_search_suitable_item"]')
+el = driver.find_elements(By.LINK_TEXT)
 
+for i in element:
+    CYCLE_INDEX += 1
+    print(CYCLE_INDEX, i.text)
+driver.quit()
 
-def getting_html():
-    response = requests.get('https://hh.ru/search/vacancy',
-                            params=params, cookies=cookies, headers=headers)
-
-    with open("index1.html", "w", encoding="utf-8") as file:
-        file.write(response.text)
-    return response
-
-
-response = getting_html()
-
-html_string = response.text
-root = html.fromstring(html_string)
-
-parse = root.xpath(
-    '//main[@class="vacancy-serp-content"]//span[@data-page-analytics-event="vacancy_search_suitable_item"]//@href')
-
-for i, href in enumerate(parse, start=1):
-    text_content = str(href)  # Получить текстовое содержимое текущего элемента
-    print(f'{i}: {text_content}')
-
-# # Выполняем XPath-запрос для получения значения элемента
-# value = root.xpath('/root/element1/text()')
-# print(value[0])  # Выводит "Value 1"
-
-# soup = bs(response.content, 'html.parser')
-
-# title = soup.find_all('a', class_="serp-item__title")
-
-# for string in title:
-#     current_string = string.text
-#     print(current_string)
-#     if current_string:
-#         regexp = re.search(
-#             r"(Python|python|junior python|Junior Python|junior Python|Junior python)", current_string)
-#         print(regexp)
-
-# get_respond_button = soup.find_all(
-#     class_="bloko-button bloko-button_kind-success bloko-button_scale-small")
-
-
-# Добавить отображение опыта
-# Добавить логин(опциональный)
-# Сделать фейковый аккаунт, сделать фейковое резюме с гпт
-# Добавить функционал спама (отклика)
-# Проспамить долбоебов
+if __name__ == "__main__":
+    pass
