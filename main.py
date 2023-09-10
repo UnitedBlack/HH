4ac4ff75c3fe16fb431119ab62e83689783de8d2
UnitedBlack <91415015+UnitedBlack@users.noreply.github.com>
2023-09-10 19:25:20 +0300

    asd

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ driver = webdriver.Chrome()
 
 driver.get("https://hh.ru/search/vacancy?text=Python&salary=&ored_clusters=true&enable_snippets=true&area=1")
 time.sleep(5)
-element = driver.find_elements(
-    By.XPATH, '//span[@data-page-analytics-event="vacancy_search_suitable_item"]')
-el = driver.find_elements(By.LINK_TEXT)
+# element = driver.find_elements(
+#     By.XPATH, '//span[@data-page-analytics-event="vacancy_search_suitable_item"]')
+get_vacancy_title = driver.find_elements(By.CLASS_NAME, 'serp-item__title')  # 
 
-for i in element:
+for i in get_vacancy_title:
     CYCLE_INDEX += 1
     print(CYCLE_INDEX, i.text)
 driver.quit()
